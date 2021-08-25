ba5c46721985c36045d50404565174bd89d0924f
CZ <cz.theng@gmail.com>
2021-08-25 10:55:10 +0800

    [TASK] add subgraph

---
+++ cryptoblades/CryptoBlades/create.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+graph init \
+  --product subgraph-studio \
+  --from-contract 0x39bea96e13453ed52a734b6aceed4c41f57b2271 \
+  --network bsc \
+  --abi ./CryptoBlades.abi \
+  cryptoblades subgraph
