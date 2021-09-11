f346c957158f4aa3eec948baa7d61f6de9dab07d
Ubuntu <ubuntu@ip-172-31-13-84.ap-east-1.compute.internal>
2021-09-11 22:13:00 +0800

    [INIT] gamefidata

---
+++ gamefidata/cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	_version bool
+)
+
+var rootCMD = &cobra.Command{
+	Use:   "gamefidata",
+	Short: "start an gamefidata",
+	Long:  `start an gamefidata`,
+	Run:   _main,
+}
+
+func init() {
+	rootCMD.PersistentFlags().BoolVarP(&_version, "version", "v", false, "print version of gamefidata")
+
+}
+
+func main() {
+	rootCMD.Execute()
+}
+
+func dumpVersion() {
+	fmt.Printf("%s\n", "0.1.0")
+}
+
+func _main(cmd *cobra.Command, args []string) {
+	if _version {
+		dumpVersion()
+		return
+	}
+
+}
